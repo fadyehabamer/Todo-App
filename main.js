6adfe0dce1030a3b6ef2bc37282e4e25b88f7763
fadyehabamer <fadyamer45@gmail.com>
2021-07-15 21:28:01 +0200

    Update main.js

--- main.js
+++ main.js
@@ -18,10 +18,8 @@ addbtn.onclick = () => {
               title: 'Can not make Empty Task !',
               width: 600,
               padding: '3em',
-              background: '#fff url(/images/trees.png)',
               backdrop: `
                 rgba(0,0,123,0.4)
-                url("/images/nyan-cat.gif")
                 left top
                 no-repeat
               `
